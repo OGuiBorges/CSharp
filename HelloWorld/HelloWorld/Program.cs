fae0a1426ac3fa79f03445b775b255ebcd4e7b57
Guilherme Borges <guilhermebborges04m2@gmail.com>
2022-09-27 13:52:24 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Hello, World!");
+
+//Váriaveis
+int i = 10;
+float f = 10.5f;
+double d = 10.5d;
+decimal c = 30.5m;
+bool b = true;
+char c2 = 'a';
+string s = "Guilherme";
+var cidade = "Blumenal";
+var idade = 30;
+var descricao = "Coco arroz";
+idade = 55;
+const string pais = "Brasil";
+f = idade;
+f = (float)c;
+
+
+//Operadores 
+//Aritméticos
+int adicao = 5 + 3;
+int subtracao = 5 - 3;
+int multplicacao = 5 * 3;
+int divisao = 5 / 3;
+int expr = (1 + 2) * 5 - 2 / 2;
+
+//Relacionais
+string cidade1 = "Blumenau";
+string cidade2 = "Teresopolis";
+bool ret1 = cidade1 == cidade2;
+bool ret2 = cidade1 != cidade2;
+int j = 2;
+int i = 5;
+bool ret3 = i == j;
+bool ret4 = i != j;
+bool ret5 = i > j;
+bool ret6 = i >= j;
+bool ret7 = i < j;
+bool ret8 = i <= j; 
+
+//Lógicos
+bool ret9 = i < j && cidade1 != cidade2;
+bool ret10 = i < j || cidade1 == cidade2;
+bool ret11 = !(i < j);
+
